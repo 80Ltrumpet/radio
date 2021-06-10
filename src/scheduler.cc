09477ff58d8ad683b5c30c48080c5067ea81f122
Andrew Lehmer <andrew.lehmer@gmail.com>
2021-06-10 11:46:17 -0400

    More scheduler cleanup
    Removed the completely unused current_task_ and fixed a bug in the spin
    detection logic.

--- src/scheduler.cc
+++ src/scheduler.cc
@@ -24,7 +24,6 @@ constexpr uint8_t kMaxTasks{ANDRUIO_MAX_TASKS};
 
 ScheduledTask tasks_[kMaxTasks]{};
 uint8_t task_count_{};
-uint8_t current_task_{kMaxTasks};
 
 uint8_t get_task_count() {
   return task_count_ < kMaxTasks ? task_count_ : kMaxTasks;
@@ -56,44 +55,42 @@ void Run() {
   const auto kTaskCount{get_task_count()};
   auto spin_us{Timer::Micros() + kSpinTimeoutUs};
   for (;;) {
-    bool spinning{false};
-    uint64_t next_until{};
-    for (current_task_ = 0; current_task_ < kTaskCount; ++current_task_) {
-      auto& task{tasks_[current_task_]};
+    bool spinning{true};
+    auto next_until{Task::kPause};
+    for (uint8_t i{}; i < kTaskCount; ++i) {
+      auto& task{tasks_[i]};
       // Skip tasks that are undefined, paused, or need to wait.
       if (!task.runner || task.until == Task::kPause) continue;
       if (Timer::Millis() < task.until) {
-        if (next_until == 0 || task.until < next_until) next_until = task.until;
+        // next_until is only meaningful when spinning.
+        if (spinning && (next_until == 0 || task.until < next_until)) {
+          next_until = task.until;
+        }
         continue;
       }
+      // We are not spinning if a task is run.
       spinning = false;
-      // TODO: Need to handle long periods.
       task.runner();
       // The task may pause itself while running.
       if (task.until != Task::kPause) {
         task.until = Timer::Millis() + task.period;
-        if (next_until == 0 || task.until < next_until) next_until = task.until;
       }
     }
 
     // Sleep if no tasks have run for a while.
     if (spinning) {
-      if (Timer::Micros() >= spin_us) {
-        if (next_until == 0) {
-          Power::Sleep();
-        } else if (auto now{Timer::Millis()}; now < next_until) {
-          auto sleep_ms{next_until - now};
-          Power::Sleep(sleep_ms < UINT16_MAX ? static_cast<uint16_t>(sleep_ms)
-                                             : UINT16_MAX);
-        }
-        // Otherwise, we just happened to hit the spin timeout before the timer
-        // rolled (extremely unlikely).
-        goto reset_spin_us;
+      if (Timer::Micros() < spin_us) continue;
+      if (next_until == 0) {
+        Power::Sleep();
+      } else if (auto now{Timer::Millis()}; now < next_until) {
+        auto sleep_ms{next_until - now};
+        Power::Sleep(sleep_ms < UINT16_MAX ? static_cast<uint16_t>(sleep_ms)
+                                           : UINT16_MAX);
       }
-    } else {
-    reset_spin_us:
-      spin_us = Timer::Micros() + kSpinTimeoutUs;
+      // Otherwise, we just happened to hit the spin timeout before the timer
+      // rolled (extremely unlikely).
     }
+    spin_us = Timer::Micros() + kSpinTimeoutUs;
   }
 }
 
